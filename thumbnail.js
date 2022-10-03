b5b73ba6080918f6e72bdb2d97ed4516ee0057bf
Muwu27 <114585082+Muwu27@users.noreply.github.com>
2022-10-03 20:33:35 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,3 @@
-let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click", function() {
-    targetImage.classList.remove("small");    
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 你的所有代码都应该写在这里
 });
-
-
